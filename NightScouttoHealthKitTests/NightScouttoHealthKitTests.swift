0d1faaf3a0f917327e36ad819966060b152cd3cf
pallagip <teaching.patrik.pallagi@gmail.com>
2025-05-09 22:05:30 +0200

    Initial Commit

---
+++ NightScouttoHealthKitTests/NightScouttoHealthKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  NightScouttoHealthKitTests.swift
+//  NightScouttoHealthKitTests
+//
+//  Created by Patrick Pallagi on 2025-05-09.
+//
+
+import Testing
+@testable import NightScouttoHealthKit
+
+struct NightScouttoHealthKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
